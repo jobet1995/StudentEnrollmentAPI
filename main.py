81b3f8ec49457195a34460909dcfc896812bb87e
jobet1995 <87087925+jobet1995@users.noreply.github.com>
2023-10-28 09:29:14 +0000

    modified

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 from flask import Flask, Blueprint
-from students import students
-from teachers import teachers
-from subjects import subjects
-from programs_courses import programs_courses
-from enrollments import enrollments
-from communication_logs import communication_logs
-from admission_decisions import admission_decisions
-from prospective_students import prospective_students
-from events_activities import events_activities
-from document_management import document_management
-from users import users
+from students_api import students_api
+from teachers_api import teachers_api
+from subjects_api import subjects_api
+from programs_courses_api import programs_courses_api
+from enrollments_api import enrollments_api
+from communication_logs_api import communication_logs_api
+from admission_decisions_api import admission_decisions_api
+from prospective_students_api import prospective_students_api
+from events_activities_api import events_activities_api
+from document_management_api import document_management_api
+from users_api import users_api
 
 app = Flask(__name__)
 
-
-app.register_blueprint(students, url_prefix='/students')
-app.register_blueprint(teachers, url_prefix='/teachers')
-app.register_blueprint(subjects, url_prefix='/subjects')
-app.register_blueprint(programs_courses, url_prefix='/programs-courses')
-app.register_blueprint(enrollments, url_prefix='/enrollments')
-app.register_blueprint(communication_logs, url_prefix='/communication-logs')
-app.register_blueprint(admission_decisions, url_prefix='/admission-decisions')
-app.register_blueprint(prospective_students, url_prefix='/prospective-students')
-app.register_blueprint(events_activities, url_prefix='/events-activities')
-app.register_blueprint(document_management, url_prefix='/document-management')
-app.register_blueprint(users, url_prefix='/users')
+app.register_blueprint(students_api, url_prefix='/students')
+app.register_blueprint(teachers_api, url_prefix='/teachers')
+app.register_blueprint(subjects_api, url_prefix='/subjects')
+app.register_blueprint(programs_courses_api, url_prefix='/programs-courses')
+app.register_blueprint(enrollments_api, url_prefix='/enrollments')
+app.register_blueprint(communication_logs_api, url_prefix='/communication-logs')
+app.register_blueprint(admission_decisions_api, url_prefix='/admission-decisions')
+app.register_blueprint(prospective_students_api, url_prefix='/prospective-students')
+app.register_blueprint(events_activities_api, url_prefix='/events-activities')
+app.register_blueprint(document_management_api, url_prefix='/document-management')
+app.register_blueprint(users_api, url_prefix='/users')
 
 if __name__ == '__main__':
-      app.run(debug=True)
+    app.run(debug=True)
