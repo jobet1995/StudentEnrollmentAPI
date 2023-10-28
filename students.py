448f8172484a279914ca658e619e2a491f57a0ac
jobet1995 <87087925+jobet1995@users.noreply.github.com>
2023-10-28 10:05:07 +0000

    add post method

--- students.py
+++ students.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import sqlite3
 
-app = Flask('students',__name__)
+app = Flask('students', __name__)
 
 def connect_to_database():
     try:
@@ -12,6 +12,30 @@ def connect_to_database():
         return None
 
 class StudentsResource:
+    def post(self):
+      try:
+        data = request.get_json()
+        
+        if data:
+          conn = connect_to_database()
+
+          if conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO students (name, contact_phone, contact_email, contact_address, application_date, application_status, program_course, test_scores, transcripts, recommendation_letters, application_fee_payment_status, application_essays, application_reviewer) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                           (data.get('name'), data.get('contact_phone'), data.get('contact_email'), data.get('contact_address'), data.get('application_date'), data.get('application_status'), data.get('program_course'), data.get('test_scores'), data.get('transcripts'), data.get('recommendation_letters'), data.get('application_fee_payment_status'), data.get('application_essays'), data.get('application_reviewer')))
+            conn.commit()
+            cursor.execute("SELECT last_insert_rowid()")
+            student_id = cursor.fetchone()[0]
+            conn.close()
+            return jsonify({'message': 'Student inserted successfully', 'student_id': student_id}), 201
+          else:
+            return jsonify({'message': 'Error connecting to the database'}), 500
+        else:
+          return jsonify({'error': 'Invalid request data'}), 400
+          
+      except Exception as e:
+        return jsonify({'error': str(e)}), 500
+        
     def get(self, student_id):
         try:
             conn = connect_to_database()
@@ -81,6 +105,8 @@ class StudentsResource:
 
 students_resource = StudentsResource()
 
+
+
 @app.route('/students/<int:student_id>', methods=['GET'])
 def get_student(student_id):
     return students_resource.get(student_id)
@@ -92,6 +118,10 @@ def update_student(student_id):
 @app.route('/students/<int:student_id>', methods=['DELETE'])
 def delete_student(student_id):
     return students_resource.delete(student_id)
+
+@app.route('/students', methods=['POST'])
+def create_student():
+  return students_resource.post()
 
 if __name__ == '__main__':
     app.run(debug=True)
